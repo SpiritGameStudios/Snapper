e2f91968bb8cee9257ce2fe3a67a33dff6e5420e
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-08-04 12:36:01 +1000

    fix: fix panorama viewer

--- src/client/java/dev/spiritstudios/snapper/gui/PanoramaViewerScreen.java
+++ src/client/java/dev/spiritstudios/snapper/gui/PanoramaViewerScreen.java
@@ -13,6 +13,8 @@ import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
+import net.minecraft.util.math.MathHelper;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,76 +24,75 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
-
-import static dev.spiritstudios.snapper.Snapper.MODID;
 
 public class PanoramaViewerScreen extends Screen {
-    private static final Identifier LOADING_BACKGROUND = Identifier.of(MODID, "textures/gui/background.png");
     protected static final CubeMapRenderer PANORAMA_RENDERER = new CubeMapRenderer(Identifier.ofVanilla("screenshots/panorama/panorama"));
     protected static final RotatingCubeMapRenderer PANORAMA_RENDERER_CUBE = new RotatingCubeMapRenderer(PANORAMA_RENDERER);
     private static final MinecraftClient client = MinecraftClient.getInstance();
 
     private final Path iconPath;
     private final String title;
+    private boolean doBackgroundFade = true;
+    private long backgroundFadeStart;
     private boolean loaded = false;
+    private float backgroundAlpha;
 
     protected PanoramaViewerScreen(String title) throws IOException {
         super(Text.translatable("menu.snapper.viewermenu"));
-        Snapper.LOGGER.info(String.valueOf(this.loaded));
         this.title = title;
+
         this.iconPath = new File(client.runDirectory + "screenshots/panorama").toPath();
         this.load();
-        this.loaded = true;
     }
 
     private void load() {
         List<File> panorama = this.loadPanorama();
         if (panorama == null) return;
-        for (File face : panorama) {
+
+        panorama.parallelStream().forEach(face -> {
             ScreenshotIcon icon = ScreenshotIcon.forPanoramaFace(client.getTextureManager(), face.getName());
             this.loadIcon(icon, face.getName(), Path.of(face.getPath()));
-        }
+        });
+
+        this.loaded = true;
     }
 
     private void loadIcon(ScreenshotIcon icon, String fileName, Path filePath) {
-        CompletableFuture.runAsync(() -> {
-            if (filePath == null || !Files.isRegularFile(filePath)) {
-                icon.destroy();
-                return;
-            }
+        if (filePath == null || !Files.isRegularFile(filePath)) {
+            icon.destroy();
+            return;
+        }
 
-            try (InputStream inputStream = Files.newInputStream(filePath)) {
-                icon.load(NativeImage.read(inputStream));
-            } catch (IOException error) {
-                Snapper.LOGGER.error("Invalid face for panorama {}", fileName, error);
-            }
-        });
+        try (InputStream inputStream = Files.newInputStream(filePath)) {
+            icon.load(NativeImage.read(inputStream));
+        } catch (IOException error) {
+            Snapper.LOGGER.error("Invalid face for panorama {}", fileName, error);
+        }
     }
 
+    @Nullable
     private List<File> loadPanorama() {
         File panoramaDir = new File(client.runDirectory, "screenshots/panorama");
         List<File> panoramaFaces;
-        if (!Files.exists(panoramaDir.toPath())) {
-            return null;
-        } else {
-            File[] faceFiles = panoramaDir.listFiles();
-            panoramaFaces = new ArrayList<>(List.of(faceFiles == null ? new File[0] : faceFiles));
-            panoramaFaces.removeIf(file -> {
-                if (Files.isDirectory(file.toPath())) return true;
-                String fileType;
+        if (!Files.exists(panoramaDir.toPath())) return null;
 
-                try {
-                    fileType = Files.probeContentType(file.toPath());
-                } catch (IOException e) {
-                    Snapper.LOGGER.error("Couldn't load panorama list", e);
-                    return true;
-                }
+        File[] faceFiles = panoramaDir.listFiles();
+        panoramaFaces = new ArrayList<>(List.of(faceFiles == null ? new File[0] : faceFiles));
+        panoramaFaces.removeIf(file -> {
+            if (Files.isDirectory(file.toPath())) return true;
+            String fileType;
 
-                return !Objects.equals(fileType, "image/png");
-            });
-            return panoramaFaces;
-        }
+            try {
+                fileType = Files.probeContentType(file.toPath());
+            } catch (IOException e) {
+                Snapper.LOGGER.error("Couldn't load panorama list", e);
+                return true;
+            }
+
+            return !Objects.equals(fileType, "image/png");
+        });
+
+        return panoramaFaces;
     }
 
     @Override
@@ -114,23 +115,33 @@ public class PanoramaViewerScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        Snapper.LOGGER.info(String.valueOf(this.loaded));
-        super.render(context, mouseX, mouseY, delta);
-        super.renderPanoramaBackground(context, delta);
-        if (!this.loaded) {
-            super.renderPanoramaBackground(context, delta);
-        } else {
-            this.renderPanoramaBackground(context, delta);
+        if (this.backgroundFadeStart == 0L && this.doBackgroundFade)
+            this.backgroundFadeStart = Util.getMeasuringTimeMs();
+
+        if (doBackgroundFade) {
+            float progress = (float) (Util.getMeasuringTimeMs() - this.backgroundFadeStart) / 2000.0F;
+
+            if (progress > 1.0F) {
+                this.doBackgroundFade = false;
+                backgroundAlpha = 1.0F;
+            } else {
+                progress = MathHelper.clamp(progress, 0.0F, 1.0F);
+                backgroundAlpha = MathHelper.clampedMap(progress, 0.0F, 0.5F, 0.0F, 1.0F);
+            }
         }
-        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
+
+        super.render(context, mouseX, mouseY, delta);
+
+        if (this.loaded) this.renderPanoramaBackground(context, delta);
+        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0xffffff);
     }
 
     @Override
-    public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta) {
-    }
+    public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta) { }
 
     @Override
     protected void renderPanoramaBackground(DrawContext context, float delta) {
-        PANORAMA_RENDERER_CUBE.render(context, this.width, this.height, 1, delta);
+        if (!this.loaded) return;
+        PANORAMA_RENDERER_CUBE.render(context, this.width, this.height, this.backgroundAlpha, delta);
     }
 }
