bf8040ced649f824da5472c56e22b4f2b14193e7
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-08-05 15:12:57 +1000

    chore: clean up panorama file storage logic

--- src/client/java/dev/spiritstudios/snapper/mixin/TitleScreenMixin.java
+++ src/client/java/dev/spiritstudios/snapper/mixin/TitleScreenMixin.java
@@ -3,7 +3,6 @@ package dev.spiritstudios.snapper.mixin;
 import dev.spiritstudios.snapper.gui.ScreenshotScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextIconButtonWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -16,13 +15,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static dev.spiritstudios.snapper.Snapper.MODID;
 
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin extends Screen {
+public abstract class TitleScreenMixin extends Screen {
+    @Unique
+    private static final Identifier SNAPPER_BUTTON_ICON = Identifier.of(MODID, "screenshots/screenshot");
 
     protected TitleScreenMixin(Text title) {
         super(title);
     }
-    @Unique
-    private static final Identifier SNAPPER_BUTTON_ICON = Identifier.of(MODID, "screenshots/screenshot");
 
     @Inject(
             method = "init",
@@ -36,12 +35,9 @@ public class TitleScreenMixin extends Screen {
         this.addDrawableChild(
                 TextIconButtonWidget.builder(
                         Text.translatable("button.snapper.screenshots"),
-                        button -> {
-                            this.client.setScreen(new ScreenshotScreen((TitleScreen)((Object)this)));
-                        },
+                        button -> this.client.setScreen(new ScreenshotScreen((TitleScreen) ((Object) this))),
                         true
                 ).width(20).texture(SNAPPER_BUTTON_ICON, 15, 15).build()
         ).setPosition(this.width / 2 - 124, y + spacingY);
-
     }
 }
