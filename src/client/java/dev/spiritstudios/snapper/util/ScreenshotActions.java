931c313c5b18a08eda41cd318dda911aa00c0672
worldwidepixel <worldwidepxl@gmail.com>
2025-10-20 22:29:48 -0700

    fix: fix #48 with MAGIC

--- src/client/java/dev/spiritstudios/snapper/util/ScreenshotActions.java
+++ src/client/java/dev/spiritstudios/snapper/util/ScreenshotActions.java
@@ -17,53 +17,57 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class ScreenshotActions {
-	public static void deleteScreenshot(Path path, Screen screen) {
-		if (!Files.exists(path)) return;
+    public static void deleteScreenshot(Path path, Screen screen) {
+        if (!Files.exists(path)) return;
 
-		MinecraftClient client = MinecraftClient.getInstance();
-		client.setScreen(
-				new ConfirmScreen(
-						confirmed -> {
-							if (confirmed) {
-								client.setScreen(new ProgressScreen(true));
-								try {
-									Files.deleteIfExists(path);
-								} catch (IOException e) {
-									Snapper.LOGGER.error("Failed to delete file", e);
-								}
-							}
-							client.setScreen(screen);
-						},
-						Text.translatable("text.snapper.delete_question"),
-						Text.translatable("text.snapper.delete_warning", path.getFileName()),
-						Text.translatable("button.snapper.delete"),
-						ScreenTexts.CANCEL
-				)
-		);
-	}
+        MinecraftClient client = MinecraftClient.getInstance();
+        client.setScreen(
+                new ConfirmScreen(
+                        confirmed -> {
+                            if (confirmed) {
+                                client.setScreen(new ProgressScreen(true));
+                                try {
+                                    Files.deleteIfExists(path);
+                                } catch (IOException e) {
+                                    Snapper.LOGGER.error("Failed to delete file", e);
+                                }
+                            }
+                            client.setScreen(screen);
+                        },
+                        Text.translatable("text.snapper.delete_question"),
+                        Text.translatable("text.snapper.delete_warning", path.getFileName()),
+                        Text.translatable("button.snapper.delete"),
+                        ScreenTexts.CANCEL
+                )
+        );
+    }
 
-	public static void renameScreenshot(Path screenshot, String newName) {
-		if (Files.exists(screenshot)) {
-			try {
-				Files.move(screenshot, screenshot.getParent().resolve(newName));
-			} catch (IOException e) {
-				Snapper.LOGGER.error("Failed to rename file", e);
-			}
-		}
-	}
+    public static void renameScreenshot(Path screenshot, String newName) {
+        if (Files.exists(screenshot)) {
+            try {
+                Files.move(screenshot, screenshot.getParent().resolve(newName));
+            } catch (IOException e) {
+                Snapper.LOGGER.error("Failed to rename file", e);
+            }
+        }
+    }
 
-	public static List<Path> getScreenshots() {
-		try (Stream<Path> stream = Files.list(SnapperUtil.getConfiguredScreenshotDirectory())) {
-			return stream.filter(path ->
-							!Files.isDirectory(path) && SafeFiles.isContentType(path, "image/png", ".png"))
-					.sorted(Comparator.<Path>comparingLong(path ->
-									SafeFiles.getLastModifiedTime(path)
-											.map(FileTime::toMillis)
-											.orElse(0L))
-							.reversed())
-					.toList();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public static List<Path> getScreenshots() {
+        Path screenshotDirectory = SnapperUtil.getConfiguredScreenshotDirectory();
+        if (!Files.exists(screenshotDirectory)) {
+            return List.of();
+        }
+        try (Stream<Path> stream = Files.list(screenshotDirectory)) {
+            return stream.filter(path ->
+                            !Files.isDirectory(path) && SafeFiles.isContentType(path, "image/png", ".png"))
+                    .sorted(Comparator.<Path>comparingLong(path ->
+                                    SafeFiles.getLastModifiedTime(path)
+                                            .map(FileTime::toMillis)
+                                            .orElse(0L))
+                            .reversed())
+                    .toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
