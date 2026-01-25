306e74dacd8f687916955fafabd184e67da6cec4
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2026-01-25 11:53:11 +1100

    chore: Update to Greenhouse Config 3.0.0-alpha.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 
 	modCompileOnlyApi(libs.greenhouse.config.api) {
 		attributes {
-			attribute(mappingsAttribute, "mojmap") // Use Mojmap build for compilation. Fabric Loom Remap is bugged at this moment.
+			attribute(mappingsAttribute, "intermediary")
 		}
 	}
 	modRuntimeOnly(libs.greenhouse.config)
