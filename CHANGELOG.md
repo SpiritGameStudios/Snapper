3741e23dcebe216d2d28900866312683eb48a339
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2026-07-09 12:52:13 +1000

    Fix panorama viewer "Open Folder"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@
 ### Bug Fixes
 - Fix a bug where deleting a screenshot from the screenshot viewer would not close the screenshot viewer, allowing you to crash the game by interacting with any button.
 - Fix several unintended behaviours relating to the Terms of Service screen and upload button.
+- Fix the "Open Folder" button in the panorama viewer screen. 
--- src/client/java/dev/spiritstudios/snapper/gui/screen/PanoramaViewerScreen.java
+++ src/client/java/dev/spiritstudios/snapper/gui/screen/PanoramaViewerScreen.java
@@ -83,7 +83,7 @@ public class PanoramaViewerScreen extends Screen {
             return;
         }
 
-        Path panoramaPath = SnapperUtil.getConfiguredScreenshotDirectory().resolve("screenshots", "panorama");
+        Path panoramaPath = SnapperUtil.getConfiguredScreenshotDirectory().resolve("panorama");
         addRenderableWidget(Button.builder(Component.translatable("button.snapper.folder"), _ -> {
             Util.getPlatform().openPath(panoramaPath);
         }).bounds(width / 2 - 150 - 4, height - 32, 150, 20).build());
